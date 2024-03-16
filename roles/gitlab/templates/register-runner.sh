cecaca987994f6caf0f67845b805a30b14d046d5
Jean-Sebastien Landry <jean-sebastien.landry.6@ulaval.ca>
2024-03-15 22:02:10 -0400

    initial commit roles/gitlab/

---
+++ roles/gitlab/templates/register-runner.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#TOKEN=<runner-token>
+RUNNER_CONTAINER=gitlab_runner_1
+GITLAB_URL=https://gitlab.{{ docker_apps_domain }}
+DOCKER_NETWORK=bridge
+
+# register runner
+docker exec -it ${RUNNER_CONTAINER} \
+       gitlab-runner register \
+       --non-interactive \
+       --token ${TOKEN} \
+       --url ${GITLAB_URL} \
+       --description "Docker Runner" \
+       --executor docker \
+       --docker-image docker:stable \
+       --docker-network-mode ${DOCKER_NETWORK} \
+       --docker-allowed-pull-policies always \
+       --docker-allowed-pull-policies if-not-present \
+       --docker-allowed-pull-policies never \
+       --docker-volumes "/cache" \
+       --docker-volumes "/run/user/{{ docker_rootless_user.uid }}/docker.sock:/var/run/docker.sock" \
+       --docker-volumes "/tmp/builds:/tmp/builds" \
+       --builds-dir "/tmp/builds"
+
+# Optional, but recommended: Set the builds directory to /tmp/builds, so job
+# artifacts are periodically purged from the runner host. If you skip this step,
+# you must clean up the default builds directory (/builds) yourself.
